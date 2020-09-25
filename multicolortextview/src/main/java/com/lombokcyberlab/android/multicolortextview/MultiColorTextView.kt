dce42953ab6c1a942d7b4b5238ea24c2d5a11186
Subbramanian Lakshmanan <subbramanil@gmail.com>
2020-09-24 21:40:00 -0500

    feat: cleanup, refactor and improve the code
    - Addresses Kotlin lint warnings
    - Remove invalid attribute in LinearLayout in the sample code
    - Cleans up unnecessary try-catch blocks and wraps the entire reading attributes into single try-catch block
    - Applies default values that avoids crashing & preserves the logic
    - Adds Kotlin Core KTX library that adds few usable extention functions
    
    **Note:**
    
    1. My understandig fo the additional try..catch blocks is to provide flexiblity for the library user to skip out few attributes and avoid crashing the app. While the intention is good, the recommended practice is to let the library user to know what attributes are mandatory, for ex., *text* to be displayed. This can be easily achieved using the `getStringOrThrow()` extention function from Kotlin KTX library. Similiar functions are available for other attributes as well.
    2. In order to avoid crashes due to missing declaration of attributes by the library user, default values can be used
    3. The idea of letting the user use either **colorScheme** or **colors** is a great idea. The flexibility can be achieved by the combination of default value for colors and the order of checking for **colors** attribute first before checking for **colorScheme**.
      - If the user declares **colors** not **colorScheme**, the user declared color is set, the color scheme is 0 and ignored.
      - If the user declares **colorScheme** not **colors**, the colors is set to default (to avoid crash & additional try-catch block), and overridden by the color scheme logic.
    4. Catching the root class exception is not recommended. Considering this is a library, the recommended way is to throw appropriate exceptions to inform the library user to avoid declaring the attributes.

--- multicolortextview/src/main/java/com/lombokcyberlab/android/multicolortextview/MultiColorTextView.kt
+++ multicolortextview/src/main/java/com/lombokcyberlab/android/multicolortextview/MultiColorTextView.kt
@@ -7,23 +7,21 @@ import android.util.AttributeSet
 import android.util.TypedValue
 import android.widget.TextView
 import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.core.content.res.getStringOrThrow
 
 /**
  * Created by deLaCious on 11/6/2017.
  */
 class MultiColorTextView : LinearLayoutCompat {
     constructor(context: Context) : this(context, null)
+
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
 
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        val arr = context.theme.obtainStyledAttributes(attrs, R.styleable.MultiColorTextView, 0, 0)
+        val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.MultiColorTextView, 0, 0)
         try {
-            colors = arr.getTextArray(R.styleable.MultiColorTextView_colors)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        try {
-            val scheme = arr.getInt(R.styleable.MultiColorTextView_colorScheme, 0)
+            colors = typedArray.getTextArray(R.styleable.MultiColorTextView_colors)?: resources.getTextArray(R.array.lively)
+            val scheme = typedArray.getInt(R.styleable.MultiColorTextView_colorScheme, 0)
             if (scheme != 0) {
                 colors = resources.getTextArray(when (scheme) {
                     1 -> R.array.lively
@@ -34,35 +32,13 @@ class MultiColorTextView : LinearLayoutCompat {
                     else -> R.array.lively
                 })
             }
-//            size = arr.getDimensionPixelSize(R.styleable.MultiColorTextView_textSize, 12).toFloat()
-        } catch (e: Exception) {
-            e.printStackTrace()
-            try {
-                colors = arr.getTextArray(R.styleable.MultiColorTextView_colors)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-
-        try {
-            size = arr.getFloat(R.styleable.MultiColorTextView_textSize, 12f)
+            text = typedArray.getStringOrThrow(R.styleable.MultiColorTextView_text)
+            size = typedArray.getFloat(R.styleable.MultiColorTextView_textSize, 12f)
+            textStyle = typedArray.getString(R.styleable.MultiColorTextView_textStyle) ?: ""
+            typedArray.recycle()
         } catch (e: Exception) {
             e.printStackTrace()
         }
-        try {
-            textStyle = arr.getString(R.styleable.MultiColorTextView_textStyle) ?: ""
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-
-        try {
-            text = arr.getString(R.styleable.MultiColorTextView_text) ?: ""
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-
-
     }
 
     init {
@@ -70,9 +46,9 @@ class MultiColorTextView : LinearLayoutCompat {
     }
 
     var textStyle: String = ""
-    var colors = Array<CharSequence>(0, { i -> i.toString() })
+    var colors = Array<CharSequence>(0) { i -> i.toString() }
     var size: Float = dp(12)
-    var _text: String = ""
+    private var _text: String = ""
     var text: String
         set(value) {
             _text = value
@@ -82,17 +58,17 @@ class MultiColorTextView : LinearLayoutCompat {
 
     private fun renderText() {
         var ctr = 0
-        _text.split("").forEachIndexed { index, s ->
-            if (!s.isEmpty()) {
+        _text.split("").forEachIndexed { _, s ->
+            if (s.isNotEmpty()) {
                 val tv = TextView(context)
-                tv.setText(s)
+                tv.text = s
                 val col: String = colors[ctr % colors.size].toString()
                 ctr++
                 tv.setTextColor(Color.parseColor(col))
                 tv.textSize = size
                 when (textStyle) {
-                    "bold" -> tv.setTypeface(Typeface.DEFAULT_BOLD)
-                    else -> tv.setTypeface(Typeface.DEFAULT)
+                    "bold" -> tv.typeface = Typeface.DEFAULT_BOLD
+                    else -> tv.typeface = Typeface.DEFAULT
                 }
 
                 addView(tv)
